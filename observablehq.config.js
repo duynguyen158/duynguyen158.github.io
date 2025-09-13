45720b627930f81bd6acb53c1d24c0e66c6f1b83
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2025-09-13 18:44:59 -0400

    [add] homepage

--- observablehq.config.js
+++ observablehq.config.js
@@ -23,12 +23,12 @@ export default {
   root: "src",
 
   // Some additional configuration options and their defaults:
-  // theme: "default", // try "light", "dark", "slate", etc.
+  theme: ["parchment", "ocean-floor"],
   // header: "", // what to show in the header (HTML)
   // footer: "Built with Observable.", // what to show in the footer (HTML)
   // sidebar: true, // whether to show the sidebar
   // toc: true, // whether to show the table of contents
-  // pager: true, // whether to show previous & next links in the footer
+  pager: false, // whether to show previous & next links in the footer
   // output: "dist", // path to the output root for build
   // search: true, // activate search
   // linkify: true, // convert URLs in Markdown to links
